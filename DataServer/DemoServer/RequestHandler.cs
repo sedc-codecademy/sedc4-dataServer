47988653a9578c8b280aa1b9be36de614ca07e6a
Wekoslav Stefanovski <swekster@gmail.com>
2016-09-21 22:23:41 +0200

    INitial network connection

--- DataServer/DemoServer/RequestHandler.cs
+++ DataServer/DemoServer/RequestHandler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DemoServer
 {
@@ -12,11 +15,45 @@ namespace DemoServer
             this.socket = socket;
         }
 
-        internal void AcceptRequest()
+        internal async Task AcceptRequest()
         {
             var clientSocket = socket.Accept();
             Console.WriteLine("Accepted call");
-            //clientSocket.Send()
+            await Task.Run(() => HandleRequest(clientSocket));
+        }
+
+        private void HandleRequest(Socket clientSocket)
+        {
+            //get request
+            var buffer = new byte[10240];
+            var receivedCount = clientSocket.Receive(buffer);
+            var request = Encoding.UTF8.GetString(buffer, 0, receivedCount);
+            Console.WriteLine(request);
+
+            //parse request
+
+            //return response
+            string myResponse = "<h1>HELLO FROM SEDC SERVER</h1>";
+            var responseBytes = Encoding.UTF8.GetBytes(myResponse);
+
+            string headers = MakeHeader(responseBytes.Length);
+            var headerBytes = Encoding.UTF8.GetBytes(headers);
+
+            clientSocket.Send(headerBytes);
+            clientSocket.Send(responseBytes);
+
+            clientSocket.Close();
+        }
+
+        private string MakeHeader(int contentLength)
+        {
+            return $@"HTTP/1.1 200 OK 
+Server: SEDC Data Web Server
+Content-Length: {contentLength}
+Connection: close
+Content-Type: text\plain
+
+";
         }
     }
 }
